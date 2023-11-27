9cc4f20fb79ada0385557c616960e202736253e7
CodingTarzan <titan-polders-0i@icloud.com>
2023-11-27 16:53:11 +0100

    deleted db connection variables and put them in a config.php outside of the webroot folder, to prevent the leaking of valueable information

--- Day 6/db.inc.php
+++ Day 6/db.inc.php
@@ -1,15 +1,7 @@
 <?php
-
-//maak connectie variabelen 
-
-$dbname = "GPTchallenges";
-$dbhost = "localhost";
-$dbuser = "bit_academy";
-$dbpass = "bit_academy";
-
-//maak DSN (Data source name) :het bevat de nodige informatie om verbinding te maken met een database.
-$dsn = "mysql:host=$dbhost;dbname=$dbname";
+//offline config.php ophalen ter voorkoming van blootstelling gevoelige informatie aan de buitenwereld.
+require_once("../../config/GPTchallenge/config.php");
 
 //maak een pdo connectie.
-$pdo = new PDO($dsn, $dbuser, $dbpass);
+$pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
