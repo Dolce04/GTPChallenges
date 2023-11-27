4a1fdd5dbe4fc8ee61eadfdc019cdc8e60ac654a
CodingTarzan <titan-polders-0i@icloud.com>
2023-11-27 14:36:37 +0100

    alle bestanden van dag 5 toegevoegd

---
+++ Day 6/db.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+//maak connectie variabelen 
+
+$dbname = "GPTchallenges";
+$dbhost = "localhost";
+$dbuser = "bit_academy";
+$dbpass = "bit_academy";
+
+//maak DSN (Data source name) :het bevat de nodige informatie om verbinding te maken met een database.
+$dsn = "mysql:host=$dbhost;dbname=$dbname";
+
+//maak een pdo connectie.
+$pdo = new PDO($dsn, $dbuser, $dbpass);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
