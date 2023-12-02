51425c056fcc277fc03aac92d15542f2cdc4bdb4
CodingTarzan <titan-polders-0i@icloud.com>
2023-12-02 14:24:31 +0100

    added status to see if message is seen or unseen, added a visual to make it more user friendly. laid down foundation for Archive function.

--- Day 6/dashboard.php
+++ Day 6/dashboard.php
@@ -2,7 +2,7 @@
 include_once("db.inc.php");
 include_once("functions.php");
 
-$resultSet = retrieveMsg($pdo,[]);
+$resultSet = retrieveAllMsg($pdo,[]);
 
 ?>
 
@@ -21,18 +21,12 @@ $resultSet = retrieveMsg($pdo,[]);
         <div class="sidebar">
             <p>Admin Dashboard</p>
             <input type="text" class="input" placeholder="         Quick Search...">
-            <a class="anchor-button" href="#"><img class="box-icon" src="img/layout-regular-24.png">Overview</a>
+            <a class="anchor-button" id="overview-button" href="#"><img class="box-icon" src="img/layout-regular-24.png">Overview</a>
             <a class="anchor-button" href="#"><img class="box-icon" src="img/setting-regular-24.png">Settings</a>
         </div>
 
     <!-- Main content -->
         <div class="main-content">
-            <!-- Header section -->
-            <div class="toolbar">
-            <!-- Toolbar section, content like search, filters, etc. -->
-
-            </div>
-
             <!-- Table Section -->
             <div class="table-container">
                 <!-- Table headers -->
@@ -41,12 +35,15 @@ $resultSet = retrieveMsg($pdo,[]);
                     <table class="table-header">
                         <tr class="table-header-row">
                             <th class="table-header-row-head">Msg ID</th>
+                            <th class="table-header-row-head" id="msgStatus">Status</th>
                             <th class="table-header-row-head">Name</th>
                             <th class="table-header-row-head">Email</th>
                             <th class="table-header-row-head">Type</th>
                             <th class="table-header-row-head">Prio</th>
                             <th class="table-header-row-head">ContactType</th>
-                            <th class="table-header-row-head">Bericht</th>
+                            <th class="table-header-row-head">Details</th>
+                            <th class="table-header-row-head">Details</th>
+
                         </tr>
                         <!-- Table Rows -->
                         <?php
@@ -55,19 +52,21 @@ $resultSet = retrieveMsg($pdo,[]);
                                 ?>
                                 <tr class="main-table-row">
                                     <td class="table-row-data"><?=htmlspecialchars($row['id']); ?></td>
+                                    <td class="table-row-data"><img src="<?php echo ($row['status'] == 'seen') ? 'img/envelope-open-regular-24.png' : 'img/envelope-regular-24.png'; ?>" alt="status"></td>
                                     <td class="table-row-data"><?=htmlspecialchars($row['naam']) ?></td>
                                     <td class="table-row-data"><?=htmlspecialchars($row['email']) ?></td>
                                     <td class="table-row-data"><?=htmlspecialchars($row['berichtType']) ?></td>
                                     <td class="table-row-data"><?=htmlspecialchars($row['prio']) ?></td>
                                     <td class="table-row-data"><?=htmlspecialchars($row['contactvoorkeur']) ?></td>
-                                    <td class="table-row-data"><?=htmlspecialchars($row['bericht']) ?></td>
+                                    <td class="table-row-data"><a href="detail.php?id=<?=$row['id']?>" class="anchor-button-small">Open</a></td>
+                                    <td class="table-row-data"><a href="detail.php?id=<?=$row['id']?>" class="anchor-button-small-archiveer">Archiveer</a></td>
                                 </tr>
                                 <?php 
                             } 
                         }  else { 
                             ?>
                             <tr>
-                                <td colspan="7">No results found</td>
+                                <td colspan="9">No results found</td>
                             </tr>
                             <?php
                         }
