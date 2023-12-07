81f7db4ce51432429472dbda13f2062bcf156b0e
CodingTarzan <titan-polders-0i@icloud.com>
2023-12-07 13:42:52 +0100

    added p element to show datetime and gave both p elements a class name

--- Day 6/detail.php
+++ Day 6/detail.php
@@ -27,7 +27,7 @@ $resultSet = receiveIdQuery($pdo,$_GET['id']);
     <!-- Main content --> 
         <div class="main-content">
             <!-- Header section -->
-            <p>Customer message</p>
+            <p class="cstMessage">Customer message</p>
             <p class="createDate">Created: <?php if (!empty($resultSet)) { echo htmlspecialchars($resultSet['gemaakt_op']);}?></p>
             <div class="top-content">
                 <!-- Toolbar section, content like search, filters, etc. -->
