fea3e9fd7ee781f667c0f03e074dd71f43c8d265
CodingTarzan <titan-polders-0i@icloud.com>
2023-12-07 02:19:20 +0100

    nothing changed

--- Day 6/dashboard_code.php
+++ Day 6/dashboard_code.php
@@ -3,12 +3,9 @@ session_start();
 include_once("db.inc.php");
 include_once("functions.php");
 
-
-
 if (isset($_POST['archive-button'])) {
 
     //Set needed Post variables into regular variables to put into the function.
     $id = $_POST['archive-button'];
     archiveRecord($pdo, $id);
-    // return header("Location:dashboard.php");
 }
