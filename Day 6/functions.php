d157c524bc4bac5a302406f5a55363184512bbef
CodingTarzan <titan-polders-0i@icloud.com>
2023-12-07 02:10:38 +0100

    addjusted the retrieveAllMsg function, so based on user action the filters and query's adjust.

--- Day 6/functions.php
+++ Day 6/functions.php
@@ -57,18 +57,108 @@ function cleaningAndValidateEmail($data)
 // PDO retrieved from db.inc.php. output the DBdata from $query request in an array in $resultSet.
 function retrieveAllMsg ($pdo, $query)
 {
-    try{
-    $query = "SELECT * FROM Berichten";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute();
-    $resultSet = $stmt->fetchALL(PDO::FETCH_ASSOC);
-    } catch (PDOException $err) {
-    file_put_contents(LOG_FILE_DB, $err->getMessage() . PHP_EOL, FILE_APPEND);
-    } catch (Exception $err) {
-    file_put_contents(LOG_FILE_ERROR, $err->getMessage() . PHP_EOL, FILE_APPEND);
+
+    //vooraf gedefinieerde waarde die geaccepteerd worden, andere waarde worden niet geaccepteerd.
+    $orderWhitelist = ['id', 'status', 'naam', 'berichtType', 'prio', 'contactvoorkeur', 'email'];
+    $sortWhitelist = ['ASC', 'DESC'];
+
+    try {
+        //Controleer of de resetFilter parameter is ingesteld.
+        if (isset($_GET['resetFilters'])) {
+            //reset de relevante sessie variabelen
+            unset($_SESSION['typeCycle']);
+            unset($_SESSION['prioCycle']);
+
+        }
+
+        //Controleer of typeCycle is aangesproken.
+        if (isset($_GET['typeCycle'])) {
+            unset($_SESSION['prioCycle']);
+            // Initialiseer typeCycle als deze nog niet is ingesteld
+            if (!isset($_SESSION['typeCycle'])) {
+                $_SESSION['typeCycle'] = '';
+            }
+        // bekijk met een switch wat de waarde is en verander naar benodigde resultaat.
+        switch ($_SESSION['typeCycle']) {
+            case '':
+                $_SESSION['typeCycle'] = 'feedback';
+                break;
+            case 'feedback':
+                $_SESSION['typeCycle'] = 'else';
+                break;
+            case 'else':
+                $_SESSION['typeCycle'] = 'question';
+                break;
+            case 'question':
+                $_SESSION['typeCycle'] = '';
+                break;
+            }
+        }
+        if (isset($_GET['prioCycle'])) {
+            unset($_SESSION['typeCycle']);
+            // Initialiseer prioCycle als deze nog niet is ingesteld
+            if (!isset($_SESSION['prioCycle'])) {
+                $_SESSION['prioCycle'] = '';
+            }
+        switch ($_SESSION['prioCycle']) {
+            case '':
+                $_SESSION['prioCycle'] = 'low';
+                break;
+            case 'low':
+                $_SESSION['prioCycle'] = 'medium';
+                break;
+            case 'medium':
+                $_SESSION['prioCycle'] = 'high';
+                break;
+            case 'high':
+                $_SESSION['prioCycle'] = '';
+                break;
+        }
+        }
+
+        //Standaard query:
+        $query = "SELECT * FROM Berichten WHERE is_gearchiveerd = FALSE";
+        // Voeg typeCycle en/of prioCycle filters toe als ze bestaan
+        if (isset($_SESSION['typeCycle']) && $_SESSION['typeCycle'] != '') {
+            $query .= " AND berichtType = '{$_SESSION['typeCycle']}'";
+        }
+        if (isset($_SESSION['prioCycle']) && $_SESSION['prioCycle'] != '') {
+            $query .= " AND prio = '{$_SESSION['prioCycle']}'";
+        }
+
+        //Toevoegen van order en sortering als deze zijn ingesteld:
+        if (isset($_GET['sortClicked'])) {
+
+        //valideren van order parameter, anders een standaard waarde meegeven 
+        $order = isset($_GET['order']) && in_array($_GET['order'], $orderWhitelist) ? $_GET['order'] : 'id'; // <- standaard kolom waarde. (kan naar eigen invullen).
+
+        // Bepaal de huidige sorteerorder gebaseerd op de sessie, met een fallback naar 'ASC'
+        $currentSort = $_SESSION['sort'] ?? 'ASC';
+
+        // Update de sessie sorteerorder voor de volgende klik
+        if ($currentSort == 'ASC') {
+            $_SESSION['sort'] = 'DESC';
+        } else {
+            $_SESSION['sort'] = 'ASC';
+        }
+
+        // Gebruik de huidige sorteerorder voor deze query
+        $query .= " ORDER BY $order $currentSort";
     }
-    return $resultSet;
+
+// Voer de query uit
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+$resultSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $err) {
+file_put_contents(LOG_FILE_DB, $err->getMessage() . PHP_EOL, FILE_APPEND);
+} catch (Exception $err) {
+file_put_contents(LOG_FILE_ERROR, $err->getMessage() . PHP_EOL, FILE_APPEND);
 }
+return $resultSet;
+}
+
 
 
 /* detail.php functions: */
@@ -113,5 +203,36 @@ function valueCheck($pdo, $resultSet){
             file_put_contents(LOG_FILE_ERROR, $err->getMessage() . PHP_EOL, FILE_APPEND);
             }
         return $resultSet;
+}
 
+function archiveRecord($pdo, $messageToArchive) {
+    $id = $messageToArchive;
+    $archiveRecord = true;
+
+    $query = "SELECT * FROM Berichten WHERE id = :id AND is_gearchiveerd = FALSE";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result && $result['status'] == 'seen'){
+
+    try {
+        $query = 'UPDATE Berichten SET
+        is_gearchiveerd=:archiveren
+        WHERE id=:id';
+
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':archiveren', $archiveRecord, PDO::PARAM_BOOL);
+        $stmt->execute();
+    } catch (PDOException $err) {
+        file_put_contents(LOG_FILE_DB, $err->getMessage() . PHP_EOL, FILE_APPEND);
+    } catch (Exception $err) {
+        file_put_contents(LOG_FILE_ERROR, $err->getMessage() . PHP_EOL, FILE_APPEND);
+    }
+} else {
+$_SESSION['foutmelding'] = "Messages must be opened before being able to archive";
+}
+return header("Location:dashboard.php");
 }
